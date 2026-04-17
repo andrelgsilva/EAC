94bded26d4296e3c07bdb9b19905166c3db01b31
Andrelgsilva <111202716+andrelgsilva@users.noreply.github.com>
2026-04-17 19:18:07 +0000

    fix: atividadepdm como pasta normal

---
+++ atividadepdm/back4app/index.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const urlBase = "https://api-tarefas-five.vercel.app/";
+const headers = {
+  "X-Parse-Application-Id": "cBYlta75GLSijbU7KUV4ZjXdhSdmxfLUuULSbKF4",
+  "X-Parse-JavaScript-Key": "QwpjPcaNXPAXHXrB4cq75su6qzFv27tmvGk5KFFj",
+};
+const headersJson = {
+  ...headers,
+  "Content-Type": "application/json",
+};
+
+export async function getTarefas() {
+  const response = await axios.get(urlBase, { headers });
+  return response.data.results;
+}
+
+export async function adicionarTarefa(novaTarefa) {
+  const response = await axios.post(
+    urlBase,
+    { descricao: novaTarefa.descricao, concluida: false },
+    { headers: headersJson }
+  );
+  return response.data;
+}
+
+export async function atualizarTarefa({ objectId, concluida }) {
+  const response = await axios.put(
+    `${urlBase}/${objectId}`,
+    { concluida },
+    { headers: headersJson }
+  );
+  return response.data;
+}
+
+export async function deletarTarefa(objectId) {
+  const response = await axios.delete(`${urlBase}/${objectId}`, { headers });
+  return response.data;
+}
+
+export async function atualizarDescricao({ objectId, descricao }) {
+  const response = await axios.put(
+    `${urlBase}/${objectId}`,
+    { descricao },
+    { headers: headersJson }
+  );
+  return response.data;
+}
